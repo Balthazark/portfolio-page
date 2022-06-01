908fa129ffc588a0436283d259a93d4adb393646
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2022-06-01 00:38:42 +0000

    more work on navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import { FaSun } from "react-icons/fa";
+import styles from "../styles/Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/"></a>
-        </li>
-        <li>
-          <a href="#aboutme"></a>Karl
-          <br />
-          Gunnarsson{" "}
-        </li>
-        <li>
-          <a href="projects">Projects</a>
-        </li>
-        <li>
-          <a href="contactme">Contact me</a>
-        </li>
-        <li>
-          <FaSun />
-        </li>
-      </ul>
-    </nav>
+    <header className={[styles["primary-header"], styles.flex].join(" ")}>
+      <span className={styles.logo}>
+        <a href="/">
+          Karl <br /> Gunnarsson
+        </a>
+      </span>
+      <nav>
+        <ul className={[styles["primary-navigation"], styles.flex].join(" ")}>
+          <li className={styles["nav-items"]}>
+            <a href="#aboutme">About me</a>
+          </li>
+          <li className={styles["nav-items"]}>
+            <a href="#projects">Projects</a>
+          </li>
+          <li className={styles["nav-items"]}>
+            <a href="#contactme">Contact me</a>
+          </li>
+          <li className={styles["nav-items"]}>
+            <a>Dark mode</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 export default Navbar;
